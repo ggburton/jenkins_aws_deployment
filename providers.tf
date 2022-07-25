84b6287b50ec4786fdefd83ba4dc5867de9a852e
Greg Burton <gburton@combitel.com.au>
2022-07-26 08:41:35 +1000

    network and instance creation complete. added ansible test states

--- providers.tf
+++ providers.tf
@@ -1,11 +1,11 @@
 provider "aws" {
   profile = var.profile
-  region  = var.region_master
+  region  = var.region-master
   alias   = "region-master"
 }
 
 provider "aws" {
   profile = var.profile
-  region  = var.region_worker
+  region  = var.region-worker
   alias   = "region-worker"
 }
--- variables.tf
+++ variables.tf
@@ -3,12 +3,27 @@ variable "profile" {
   default = "default"
 }
 
-variable "region_master" {
+variable "region-master" {
   type    = string
   default = "us-east-1"
 }
 
-variable "region_worker" {
+variable "region-worker" {
   type    = string
   default = "us-west-2"
+}
+
+variable "external-ip" {
+  type    = string
+  default = "0.0.0.0/0"
+}
+
+variable "instance-type" {
+  type    = string
+  default = "t3.micro"
+}
+
+variable "workers-count" {
+  type    = number
+  default = 1
 }
